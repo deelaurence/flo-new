268437b0cdcde2e2b08f4c60a3d1f24ab52514c2
deelaurence <laurie143pelumi@gmail.com>
2023-01-17 21:58:29 +0100

    first commit

---
+++ src/data/landing.js
@@ -0,0 +1,40 @@
+let data = [
+    {
+        name: 'Alertz',
+        image: '../images/landing-alertz.png',
+        details: 'A Crime Alert Application.',
+        year: '2020',
+    },
+    {
+        name: 'Mixed Reality',
+        image: '../images/landing-vr.png',
+        details: 'Mixed Reality Interaction.',
+        year: '2020',
+    },
+    {
+        name: 'Guild',
+        image: '../images/landing-guild.png',
+        details: 'A Web3 Community Platform.',
+        year: '2020',
+    },
+    {
+        name: 'Kodetech',
+        image: '../images/landing-kodetech.png',
+        details: 'An E-Commerce Website.',
+        year: '2020',
+    },
+    {
+        name: 'Femtech',
+        image: '../images/landing-femmina.png',
+        details: 'A FemTech Application.',
+        year: 'Coming Soon',
+    },
+    {
+        name: 'D-Exchange',
+        image: '../images/landing-dexchange.png',
+        details: 'A Liquidity Pool Design.',
+        year: 'Coming Soon',
+    },
+]
+
+export default data
